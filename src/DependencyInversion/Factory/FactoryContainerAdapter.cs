5c002d9b006d171e08ef8a63de209b79e3c2e44a
Aaron Job <aaron@gravypower.net>
2018-09-20 08:47:30 +1000

    Added Factory

---
+++ src/DependencyInversion/Factory/FactoryContainerAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DependencyInversion.CompositionRoot;
+using Sitecore.Reflection;
+
+namespace DependencyInversion.Factory
+{
+    public class FactoryContainerAdapter: IFactory
+    {
+        public object GetObject(string identifier)
+        {
+            var type = Type.GetType(identifier);
+            return Bootstrapper.Container.GetInstance(type);
+        }
+    }
+}
